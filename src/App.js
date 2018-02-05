b226580722e2e30af10f8f87ca2e99f3db7b8bd3
josephcorbin91 <joseph.corbin91@yahoo.com>
2018-02-05 15:14:44 -0800

    background image added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation.js';
 import Footer from './layouts/Footer.js';
+import BackgroundImage from './images/Banner.png';
 
+var style = {
+	width: "100%",
+	height: "400px",
+	backgroundImage: "url("+ BackgroundImage + ")"
+};
 export default class App extends Component {
+
+
   render() {
     return (
-      <div>
+      <div style = { style }>
         <Navigation />
          <Footer />
       </div>
