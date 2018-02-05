1d0e9f929c73cbfc6bb06423e37b6bb2e2724cb9
josephcorbin91 <joseph.corbin91@yahoo.com>
2018-02-05 14:39:29 -0800

    Footer completed

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Form, FormControl, Navbar, Glyphicon,
   Nav, NavItem, Well, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router';
+import '../css/footer.css'
 
 export default class Footer extends Component {
   constructor(props) {
@@ -10,40 +11,8 @@ export default class Footer extends Component {
  
   render() {
     return (
-      <div className="Navigation__container">
-        <Navbar inverse fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">React Eshop</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullLeft>
-              <NavItem >
-                App
-              </NavItem>
-              <NavItem >
-                Org
-              </NavItem>
-               <NavItem >
-                Publicity
-              </NavItem>
-              <NavItem >
-                Sponsor
-              </NavItem>
-              <NavItem >
-                Q&A
-              </NavItem>
-              
-              {this.state.logout
-                ? <NavItem onClick={this.logout} href="">
-                    Logout <Glyphicon glyph="log-out" /></NavItem>
-                : <NavItem onClick={this.login} href="">
-                    Login <Glyphicon glyph="transfer" /></NavItem>}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <div className="footer">
+       Hello
         </div>
        
     );
